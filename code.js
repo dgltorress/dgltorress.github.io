a27cf0dd9eb833ae02b5f823636fb2b10bc6d671
dgltorress <dgltorress@gmail.com>
2023-06-29 00:55:19 +0200

    dynamic experiences + adjustments

--- code.js
+++ code.js
@@ -69,25 +69,13 @@ const writeYear = ( timeElement ) => {
  * 
  * @param {HTMLElement} projectsSection Projects section.
  * @param {any[]} projects Project array.
- * @param {string} className Project class name.
- * @param {string} subsectionTitle New subsection title.
  */
 const loadProjects = (
     projectsSection,
-    projects,
-    className,
-    subsectionTitle = 'SECTION TITLE'
+    projects
 ) => {
     if( projectsSection &&
         projectsSection.tagName === 'SECTION' ){
-        // Creates subsection
-        const newSubsection = document.createElement( 'SECTION' );
-        const newHeader = document.createElement( 'H3' );
-
-        newHeader.textContent = subsectionTitle;
-
-        newSubsection.appendChild( newHeader );
-
         // Creates and appends projects.
         for( const project of projects ){
             // Creates main elements.
@@ -105,8 +93,8 @@ const loadProjects = (
             const preview = document.createElement( 'IMG' );
 
             // Sets properties.
-            container.classList.add( className );
-            info.classList.add( className );
+            container.classList.add( 'bproject' );
+            info.classList.add( 'bproject' );
 
             title.textContent = project.title;
             description.innerHTML = project.description;
@@ -129,6 +117,7 @@ const loadProjects = (
                 linkElement.setAttribute( 'target', '_blank' );
                 linkElement.textContent = link.title;
 
+                linkWrapper.classList.add( 'projlinks' );
                 linkWrapper.appendChild( linkElement );
             }
 
@@ -138,6 +127,7 @@ const loadProjects = (
                 preview.setAttribute( 'src', project.img.src );
                 preview.setAttribute( 'alt', project.img.aria );
                 preview.setAttribute( 'title', project.img.aria );
+                preview.setAttribute( 'loading', 'lazy' );
             } else {
                 console.warn( '(loadProjects) WARN: Project doesn\'t contain information on the preview (img)' );
             }
@@ -150,14 +140,112 @@ const loadProjects = (
             previewWrapper.appendChild( preview );
 
             container.append( infoWrapper, previewWrapper );
-            newSubsection.appendChild( container );
+            projectsSection.appendChild( container );
         }
-
-        projectsSection.appendChild( newSubsection );
     } else {
         console.error( '(loadProjects) ERROR: Expected a section tag, got ', projectsSection );
     }
 }
+
+/**
+ * Loads experiences into the specified container (must be a `<div>` tag).
+ * 
+ * @param {HTMLElement} experiencesSection Projects section.
+ * @param {any[]} experiences Project array.
+ * @param {string} className Class name.
+ */
+const loadExperiences = (
+    experiencesSection,
+    experiences,
+    className
+) => {
+    if( experiencesSection &&
+        experiencesSection.tagName === 'SECTION' ){
+        // Creates and appends projects.
+        for( const experience of experiences ){
+            // Creates main elements.
+            const container = document.createElement( 'DIV' );
+            const article = document.createElement( 'ARTICLE' );
+            const header = document.createElement( 'HEADER' );
+            const link = document.createElement( 'A' );
+            const footer = document.createElement( 'FOOTER' );
+
+            const heading = document.createElement( 'H3' );
+            const timeWrapper = document.createElement( 'SPAN' );
+
+            const logo = document.createElement( 'IMG' );
+
+            // Sets properties
+            container.classList.add( 'experience', className );
+
+            heading.textContent = experience.title;
+            header.appendChild( heading );
+
+            if( experience.time ){
+                const experienceTime = experience.time;
+
+                if( experienceTime.start ){
+                    const start = document.createElement( 'TIME' );
+
+                    start.textContent = experienceTime.start.title;
+                    start.setAttribute( 'datetime', experienceTime.start.datetime );
+
+                    timeWrapper.appendChild( start );
+
+                    if( experienceTime.end ){
+                        const separator = document.createTextNode( ' - ' );
+                        timeWrapper.appendChild( separator );
+                    }
+                }
+                
+                if( experienceTime.end ){
+                    const end = document.createElement( 'TIME' );
+
+                    end.textContent = experienceTime.end.title;
+                    end.setAttribute( 'datetime', experienceTime.end.datetime );
+
+                    timeWrapper.appendChild( end );
+                }
+
+                header.appendChild( timeWrapper );
+            } else {
+                console.error( '(loadExperiences) ERROR: Unable to fetch time frame from experience' );
+            }
+
+            if( experience.url ){
+                if( experience.img ){
+                    logo.setAttribute( 'src', experience.img.src );
+                    logo.setAttribute( 'alt', experience.img.aria );
+
+                    link.appendChild( logo );
+                } else {
+                    console.error( '(loadExperiences) ERROR: Unable to fetch logo from experience' );
+                }
+                
+                const linkTitle = document.createTextNode( experience.url.title );
+                link.setAttribute( 'href', experience.url.href );
+                link.setAttribute( 'title', experience.url.aria );
+                link.setAttribute( 'target', '_blank' );
+
+                link.appendChild( linkTitle );
+            } else {
+                console.error( '(loadExperiences) ERROR: Unable to fetch url from experience' );
+            }
+
+            footer.innerHTML = experience.footerHTML;
+
+            // Append elements
+            article.append( header, link, footer );
+            container.appendChild( article );
+            experiencesSection.appendChild( container );
+        }
+
+        
+    } else {
+        console.error( '(loadExperiences) ERROR: Expected a section tag, got ', experiencesSection );
+    }
+}
+
 
 // Code to execute on page load.
 window.onload = () => {
@@ -165,20 +253,46 @@ window.onload = () => {
     const projectsSection = document.getElementById( 'projects' );
 
     if( projectsSection ){
-        loadProjects(
-            projectsSection,
-            projects.student,
-            'bproject',
-            'Student'
+        const projectsSubsections = projectsSection.getElementsByTagName( 'SECTION' );
+
+        if( projectsSubsections &&
+            projectsSubsections.length  === 2 /* 3 */ ){
+            /* loadProjects(
+                projectsSubsections[ 0 ],
+                projects.work
+            ); */
+            loadProjects(
+                projectsSubsections[ 0 /* 1 */ ],
+                projects.student
+            );
+            loadProjects(
+                projectsSubsections[ 1 /* 2 */ ],
+                projects.own
+            );
+        } else {
+            console.error( 'ERROR: Failed to fetch the projects subsections (must have 3), got ', projectsSubsections );
+        }
+    } else {
+        console.error( 'ERROR: Failed to fetch the main projects section' );
+    }
+
+    // Loads the experiences.
+    const workSection = document.getElementById( 'experiences' );
+    const educationSection = document.getElementById( 'educations' );
+
+    if( workSection && educationSection ){
+        loadExperiences(
+            workSection,
+            experiences.work,
+            'work'
         );
-        loadProjects(
-            projectsSection,
-            projects.own,
-            'bproject',
-            'Personal'
+        loadExperiences(
+            educationSection,
+            experiences.education,
+            'education'
         );
     } else {
-        console.error( 'ERROR: Failed to fetch the projects section' );
+        console.error( 'ERROR: Failed to fetch either the work or educational experience sections, got ', workSection, educationSection );
     }
 
     // Writes the current year on the last TIME element.
@@ -382,4 +496,91 @@ const projects = {
             }
         }
     ]
-}
+};
+
+const experiences = {
+    work: [
+        {
+            title: 'Web developer intern',
+            time: {
+                start: {
+                    title: 'Jun 2023',
+                    datetime: '2023-06'
+                }
+            },
+            url: {
+                title: 'NTT DATA',
+                href: 'https://es.nttdata.com/',
+                aria: 'Official page of NTT DATA'
+            },
+            img: {
+                src: 'img/logo-nttdata.jpg',
+                aria: 'Logo of NTT DATA'
+            },
+            footerHTML: '<p>Providing valuable support on development tasks.</p>'
+        }
+    ],
+    education: [
+        {
+            title: 'Bachelor\'s in Multimedia Engineering',
+            time: {
+                start: {
+                    title: 'Sep 2019',
+                    datetime: '2019-09'
+                },
+                end: {
+                    title: 'Aug 2023',
+                    datetime: '2023-08'
+                }
+            },
+            url: {
+                title: 'University of Alicante',
+                href: 'https://web.ua.es/en/grados/grado-en-ingenieria-multimedia/degree-in-multimedia-engineering.html',
+                aria: 'Official page of the Bachelor\'s in Multimedia Engineering'
+            },
+            img: {
+                src: 'img/e_ua.jpg',
+                aria: 'Logo of the University of Alicante'
+            },
+            footerHTML: '<p>Currently specializing in Content Management.</p>'
+        },
+        {
+            title: 'High School Diploma',
+            time: {
+                end: {
+                    title: 'Jun 2019',
+                    datetime: '2019-06'
+                }
+            },
+            url: {
+                title: 'Alicante Salesian School',
+                href: 'https://alicante.salesianos.edu/',
+                aria: 'Official page of Alicante Salesian School'
+            },
+            img: {
+                src: 'img/e_salesianos.png',
+                aria: 'Logo of Alicante Salesian School'
+            },
+            footerHTML: '<p>Elective Social Studies subjects.</p>'
+        },
+        {
+            title: 'B2 First',
+            time: {
+                end: {
+                    title: 'Dec 2016',
+                    datetime: '2016-12'
+                }
+            },
+            url: {
+                title: 'Cambridge Assessment International Education',
+                href: 'https://blog.cambridgeinternational.org/',
+                aria: 'The Cambridge International Blog'
+            },
+            img: {
+                src: 'img/e_cambridge.jpg',
+                aria: 'Logo of Cambridge Assessment International Education'
+            },
+            footerHTML: '<p>Scored 177 — Credential ID: 16CES3970036ID</p>'
+        }
+    ]
+};
---
+++ experiences.js
@@ -0,0 +1,86 @@
+export const experiences = {
+    work: [
+        {
+            title: 'Web developer intern',
+            time: {
+                start: {
+                    title: 'Jun 2023',
+                    datetime: '2023-06'
+                }
+            },
+            url: {
+                title: 'NTT DATA',
+                href: 'https://es.nttdata.com/',
+                aria: 'Official page of NTT DATA'
+            },
+            img: {
+                src: 'img/logo-nttdata.jpg',
+                aria: 'Logo of NTT DATA'
+            },
+            footerHTML: '<p>Providing valuable support on development tasks.</p>'
+        }
+    ],
+    education: [
+        {
+            title: 'Bachelor\'s in Multimedia Engineering',
+            time: {
+                start: {
+                    title: 'Sep 2019',
+                    datetime: '2019-09'
+                },
+                end: {
+                    title: 'Aug 2023',
+                    datetime: '2023-08'
+                }
+            },
+            url: {
+                title: 'University of Alicante',
+                href: 'https://web.ua.es/en/grados/grado-en-ingenieria-multimedia/degree-in-multimedia-engineering.html',
+                aria: 'Official page of the Bachelor\'s in Multimedia Engineering'
+            },
+            img: {
+                src: 'img/e_ua.jpg',
+                aria: 'Logo of the University of Alicante'
+            },
+            footerHTML: '<p>Currently specializing in Content Management.</p>'
+        },
+        {
+            title: 'High School Diploma',
+            time: {
+                end: {
+                    title: 'Jun 2019',
+                    datetime: '2019-06'
+                }
+            },
+            url: {
+                title: 'Alicante Salesian School',
+                href: 'https://alicante.salesianos.edu/',
+                aria: 'Official page of Alicante Salesian School'
+            },
+            img: {
+                src: 'img/e_salesianos.png',
+                aria: 'Logo of Alicante Salesian School'
+            },
+            footerHTML: '<p>Elective Social Studies subjects.</p>'
+        },
+        {
+            title: 'B2 First',
+            time: {
+                end: {
+                    title: 'Dec 2016',
+                    datetime: '2016-12'
+                }
+            },
+            url: {
+                title: 'Cambridge Assessment International Education',
+                href: 'https://blog.cambridgeinternational.org/',
+                aria: 'The Cambridge International Blog'
+            },
+            img: {
+                src: 'img/e_cambridge.jpg',
+                aria: 'Logo of Cambridge Assessment International Education'
+            },
+            footerHTML: '<p>Scored 177 — Credential ID: 16CES3970036ID</p>'
+        }
+    ]
+};
